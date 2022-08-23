46aaa9d14ea4ddad80aecc69709c9294b7737b24
anikatahsin12 <bushratahsin653@gmail.com>
2022-08-23 23:35:13 +0600

    another

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -49,6 +49,39 @@ const SpecialMenu = () => (
       </div>
     </div>
   </div>
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p>Wine & Beer</p>
+      </div>
+    </div>
+  </div>
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p>Wine & Beer</p>
+      </div>
+    </div>
+  </div>
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p>Wine & Beer</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default SpecialMenu;
